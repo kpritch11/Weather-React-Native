8a4d29e1149ddd1447a5734ad7fe42bb81468ad2
kurtispritchard <kpritch11@gmail.com>
2016-06-07 18:27:15 -0500

    Minimum code to build

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// Import the code we need
+import React from 'react';
 
-import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    View
 } from 'react-native';
 
-class weather extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+// Create a react component
+var Weather = React.createClass({
+    render: function() {
+        return <View ></View>
+    }
 });
 
-AppRegistry.registerComponent('weather', () => weather);
+// Style the react component
+var styles = StyleSheet.create({
+
+});
+
+// Show the react component on the screen
+AppRegistry.registerComponent('weather', () => Weather);
