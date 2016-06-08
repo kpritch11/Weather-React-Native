62290514b2418776d2805ca88c367890fca30dbc
kurtispritchard <kpritch11@gmail.com>
2016-06-08 16:14:36 -0500

    Added api.js fetch method

--- src/api.js
+++ src/api.js
@@ -1,5 +1,21 @@
 var rootURL = 'api.openweathermap.org/data/2.5/weather?APPID=bfba5db7cce578048f894c823a59be50'
 
+var kelvinToFarenheight = function(kelvin) {
+    return Math.round((kelvin - 273.15) * 1.8 + 32) + ' ˚F'
+}
+
 module.exports = function(latitude, longitude) {
     var url = `${rootURL}&lat=${latitude}&lon=${longitude}`;
+
+    fetch(url)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(json) {
+            return {
+                city: json.name,
+                temperature: kelvinToFarenheight(json.main.temp),
+                description: json.weather[0].description
+            }
+        });
 }
